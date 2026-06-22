91468d073a58f3aded2c03df8d757507322ec880
Prabhat Pandey <prabhatpandey2150@gmail.com>
2026-06-22 16:57:36 +0530

    Update ALLOWED_HOSTS for Render

--- LMS/settings.py
+++ LMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fw9ci6r6q@y+z^-6@w(f3ogcnjh&)ksqe&9_0_+c+17!arrosc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['library-management-system-ytdy.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
